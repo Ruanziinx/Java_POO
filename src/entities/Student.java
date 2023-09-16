47ccf86ed02422da56c83ac81f54d7fab396c4b7
Ruan Ripardo <ruanpeixotorip@gmail.com>
2023-09-15 21:07:27 -0300

    commit ProgramStudent

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,2 +1,21 @@
-package entities;public class Student {
+package entities;
+
+public class Student {
+
+    public String nome;
+    public double nota1;
+    public double nota2;
+    public double nota3;
+
+    public double finalGrade(){
+        return nota1 + nota2 + nota3;
+    }
+
+    public double missingPoints(){
+        if (finalGrade() < 60)
+            return 60 - finalGrade();
+        else {
+            return 0.0;
+        }
+    }
 }
