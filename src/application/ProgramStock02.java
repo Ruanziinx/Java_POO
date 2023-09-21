2c519f92834ad0b8bcd4611577614217b7e105ad
Ruan Ripardo <ruanpeixotorip@gmail.com>
2023-09-21 17:30:43 -0300

    Commit get e set

--- src/application/ProgramStock02.java
+++ src/application/ProgramStock02.java
@@ -1,2 +1,50 @@
-package application;public class ProgramStock02 {
+package application;
+
+import entities.Product02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramStock02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("SOU GAY");
+
+            System.out.println("Enter product data: ");
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            Product02 product = new Product02(name, price);
+
+            product.setName("Computer");
+            System.out.println("Updated name: " + product.getName());
+            product.setPrice(1200.00);
+            System.out.println("Updated price: " + product.getPrice());
+
+            System.out.println(" ");
+            System.out.println("Product data: " + product);
+
+            System.out.println(" ");
+            System.out.print("Enter the number of products to be added in stock: ");
+            int quantity = sc.nextInt();
+            product.addProducts(quantity);
+
+            System.out.println(" ");
+            System.out.println("Update data: " + product);
+
+            System.out.println(" ");
+            System.out.print("Enter the number of products to be removed in stock: ");
+            quantity = sc.nextInt();
+            product.removeProducts(quantity);
+
+            System.out.println(" ");
+            System.out.println("Update data: " + product);
+
+
+            sc.close();
+    }
 }
+
